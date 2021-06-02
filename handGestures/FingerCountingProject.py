76dfbae2d543ede88ad0ed4c66e56c5bced6a79b
Simone-Smaldore <simone.smaldore@gmail.com>
2021-06-02 20:59:13 +0200

    Implementata conteggio delle dita della mano

--- handGestures/FingerCountingProject.py
+++ handGestures/FingerCountingProject.py
@@ -1,8 +1,7 @@
 import cv2
 import time
 import os
-#import HandTrackingModule as hm
-#TO DO Completare prima il progetto di Hand Tracking
+import HandTrackingModule as htm
 
 wCam, hCam = 1640, 480
 
@@ -16,16 +15,37 @@ myList.sort()
 print(myList)
 overlayList = []
 for imPath in myList:
-    image = cv2.imread(f'{folderPath}/{imPath}') # magia della formattazione f
+    image = cv2.imread(f'{folderPath}/{imPath}')
     #print(f'{folderPath}/{imPath}')
     overlayList.append(image)
 
-print(len(overlayList))
-pTime = 0
+detector = htm.HandDetector(detection_confidence=0.75)
+
+top_finger_list = [8, 12, 16, 20]
+
 while True:
     success, img = cap.read()
+    detector.draw_hands_on_image(img)
+    landmarks_list_right = detector.find_position(img, htm.RIGHT_HAND)
 
-    img[0:200, 0:200] = cv2.resize(overlayList[5], (200, 200))  #slicing per mostrare l'immagine delle dita
+    if len(landmarks_list_right) != 0:
+
+        finger_list = []
+        if landmarks_list_right[4][1] > landmarks_list_right[3][1]:
+            finger_list.append(1)
+        else:
+            finger_list.append(0)
+
+        for i in range(len(top_finger_list)):
+            if landmarks_list_right[top_finger_list[i]][2] < landmarks_list_right[top_finger_list[i] - 2][2]:
+                finger_list.append(1)
+            else:
+                finger_list.append(0)
+
+
+        num_fingers = finger_list.count(1)
+        #print(num_fingers)
+        img[0:200, 0:200] = cv2.resize(overlayList[num_fingers - 1], (200, 200))
 
 
     cv2.imshow("Image", img)
